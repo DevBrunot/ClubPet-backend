70599a598784689c8091627e1c04a4d118261058
Marcus urani <marcus.urani20@gmail.com>
2024-12-06 12:18:36 -0300

    Corrigo import

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ClubPetAPI.Data;
-using ClubPetAPI.Models;
 using System.Collections.Generic;
+using MeuProjetoAPI.Models;
 
 namespace ClubPetAPI.Controllers
 {
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using ClubPetAPI.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer("sua_string_de_conexão_aqui")); 
+            options.UseSqlServer("Server=tcp:clubpet-server.database.windows.net,1433;Initial Catalog=clubpet-db;Persist Security Info=False;User ID=thomaz-leal;Password=Azure@123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
         services.AddScoped<UsuarioRepository>(); 
 
         services.AddControllers();
